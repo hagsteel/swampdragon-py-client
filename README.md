f6d06259ba697b443124cf2138afdc44157a8302
Jonas Hagstedt <hagstedt@gmail.com>
2015-02-01 00:17:46 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# swampdragon-py-client
-A python client for swamp dragon
+# SwampDragon python client
+
+Communicate with SwampDragon through python.
+
+
+## Examples
+
+    def on_channel_message(channel, message):
+        # Only care if the channel is "test"
+        if channel == 'test':
+            output = '{} says: {}'.format(message.get('name'), message.get('message'))
+            print(output)
+
+    def on_subscribed(context, message):
+        print('-subscribed-')
+        
+    url = 'ws://localhost:9999/data'
+    client = DragonClient(url, on_channel_message=on_channel_message)
+    client.connect()
+    
+    # Subscribe to the chat router
+    client.call_router('subscribe', 'chat-route', callback=on_subscribed, channel='test')
+    
+    # Send a message as Veronica saying "Hello world"
+    client.call_router('chat', 'chat-route', callback=on_chat, name='Veronica', message='Hello world')
+    
+
+## Client functions
+
+`client.connect` connects to the server and starts listening to incoming data
+`client.disconnect` disconnects from the server
+
+`client.call_router` takes the following args:
+
+*  verb
+*  route
+*  callback=None
+*  **kwargs
+
+The `callback` is any function that takes two arguments: `context` and `message`
---
+++ pyclient/sdclient.py
@@ -0,0 +1,112 @@
+import random
+import string
+import threading
+import json
+import websocket
+
+
+channels = {}
+
+
+def message_to_json(data):
+    if isinstance(data, dict):
+        return data
+    try:
+        data = json.loads(data)
+        # Channel messages come back as escaped strings and needs to be loaded as json again (it's an odd one)
+        if not isinstance(data, dict):
+            data = json.loads(data)
+        return data
+    except:
+        return json.dumps({'message': data})
+
+
+class DragonClient(object):
+    def __init__(self, url, on_channel_message=None, on_open=None, on_exception=None):
+        self.thread = None
+        if url[-1] != '/':
+            url = '{}/'.format(url)
+        self.url = '{}{}/{}/websocket'.format(url, self._rand_int(), self._rand_string())
+        self.call_queue = []
+        self.is_connected = False
+        self.callbacks = []
+        self.on_channel_message = on_channel_message
+        self.on_exception = on_exception
+
+    def _rand_int(self):
+        return random.randint(0, 1000)
+
+    def _rand_string(self):
+        return ''.join(random.choice(string.ascii_lowercase) for _ in range(10))
+
+    def run(self):
+        self.thread = threading.Thread(target=self.ws.run_forever)
+        self.thread.start()
+
+    def connect(self):
+        self.ws = websocket.WebSocketApp(
+            self.url,
+            on_message=self.on_message,
+            on_error=self.on_error,
+            on_close=self.on_close,
+            on_open=self.on_open
+        )
+        self.run()
+
+    def disconnect(self):
+        self.ws.close()
+
+    def on_message(self, ws, message):
+        if message[0:2] != 'a[':  # This is not a message for the client
+            return
+
+        message = message_to_json(message[2:-1])
+        context = message.get('context')
+        data = message.get('data')
+
+        # Channel setup
+        channel_data = message.get('channel_data')
+        if channel_data:
+            remote_channels = channel_data.get('remote_channels') or []
+            local_channel = channel_data['local_channel']
+            for rc in remote_channels:
+                channels[rc] = local_channel
+
+        if context:
+            callback_name = context.get('client_callback_name')
+            if callback_name:
+                cb = int(callback_name[3:])
+                callback = self.callbacks.pop(int(cb))
+                if callback:
+                    callback(context, data)
+        elif 'channel' in message and self.on_channel_message:
+            self.on_channel_message(channels[message['channel']], message)
+
+    def on_error(self, ws, error):
+        if self.on_exception:
+            self.on_exception(error)
+
+    def on_close(self, ws):
+        self.is_connected = False
+
+    def on_open(self, ws):
+        self.is_connected = True
+        for f in self.call_queue:
+            self._send(f)
+
+    def _send(self, message):
+        self.ws.send(message)
+
+    def call_router(self, verb, route, callback=None, **kwargs):
+        self.callbacks.append(callback)
+        callback_id = len(self.callbacks) - 1
+        message = json.dumps({
+            'verb': verb,
+            'route': route,
+            'args': kwargs,
+            'callbackname': 'cb_{}'.format(callback_id)
+        })
+        if self.is_connected:
+            self._send(message)
+        else:
+            self.call_queue.append(message)
